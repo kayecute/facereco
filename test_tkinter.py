5690872c12aee50f16505a9994db25eba40ec431
kayecute <drneetotaku@gmail.com>
2024-04-12 08:28:29 +0700

    change part for cuda

--- test_tkinter.py
+++ test_tkinter.py
@@ -1,5 +1,7 @@
 import torch
 # Kiểm tra xem CUDA có sẵn không và thiết lập biến 'device'
+# pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121  // sử dụng khi GPU của bạn đủ mạnh, ueu tiên RTX2050 trở lên
+# thay đô đường đẫn và phiên bản cuda phù hợp với máy của bạn   
 print(torch.cuda.is_available())  # Nếu trả về True, bạn đã cài PyTorch với CUDA thành công.
    
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
